e12770801f712d06fcf1d702df168da963760c7e
dounia-elg <dounia.elg20@gmail.com>
2025-09-26 17:24:22 +0100

    fix(mealLogging): fix import issue and ensure routes/controllers work correctly

--- src/controllers/mealLoggingController.js
+++
@@ -1,5 +0,0 @@
-
-
-export const showMealLogging = (req, res) => {
-    res.render("mealLogging/mealLogging", { error: null });
-};
--- src/routes/mealLogging.js
+++
@@ -1,9 +0,0 @@
-
-import { Router } from "express";
-import { showmealLogging } from "../controllers/mealLoggingController.js";
-
-const router = Router ();
-
-router.get("/", showmealLogging);
-
-export default router;
