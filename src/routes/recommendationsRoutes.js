5fa85d19b8c515ef022a44eeed6f3d82f1323d32
dounia-elg <dounia.elg20@gmail.com>
2025-10-01 21:15:56 +0100

    Add /recommendations/api endpoint returning mock data

--- src/routes/recommendationsRoutes.js
+++ src/routes/recommendationsRoutes.js
@@ -1,9 +1,10 @@
 
 import { Router } from "express";
-import { showRecommendations } from "../controllers/recommendationsController.js";
+import { showRecommendations, listRecommendations } from "../controllers/recommendationsController.js";
 
 const router = Router ();
 
 router.get("/", showRecommendations);
+router.get("/api", listRecommendations);
 
 export default router;
