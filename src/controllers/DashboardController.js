332696a757f0617fba89ac6492198a1333c72c91
Mouad <hamideddinemouad@gmail.com>
2025-10-03 16:16:49 +0100

    final modifications

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -36,7 +36,25 @@ export const getDashboardStats = async function getDashboardStats(req, res){
        WHERE user_id = ?`,
       [userId]
     );
-        result = {totalCalories : todayCalories, all : macros, goal : max};
+    const [recent] = await pool.query(
+      `SELECT m.meal_id,
+       m.timestamp,
+       m.calories,
+       m.carbs,
+       m.proteins,
+       m.fats,
+       m.image_url,
+       fi.name AS food_name
+       FROM Meals m
+       JOIN Food_Items fi ON m.meal_id = fi.meal_id
+       WHERE m.user_id = ?
+       ORDER BY m.timestamp DESC
+       LIMIT 2;`,
+      [userId]
+    );
+    
+
+        result = {totalCalories : todayCalories, all : macros, goal : max, recentMeals : recent};
         res.render("layouts/dashboard", result);
   // console.log(result);
 }
